7361e91165f16e03454b0cde5e2bf0b6033a27bd
Noura Ghazghazi <ghazghazinoura@gmail.com>
2023-06-27 21:10:40 +0100

    Generating main Home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { NewslettersComponent } from './components/newsletters/newsletters.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { LoginComponent } from './components/login/login.component';
     HomeComponent,
     FooterComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    BannerComponent,
+    AboutComponent,
+    CoursesComponent,
+    CategoriesComponent,
+    TestimonialsComponent,
+    TeachersComponent,
+    BlogComponent,
+    NewslettersComponent
   ],
   imports: [
     BrowserModule,
